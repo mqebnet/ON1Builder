eaf56dcd833608bc9db672812f54cdc4b21d0461
John0n1 <john0n1@mitsolut.com>
2025-01-24 03:48:00 +0100

    refactor: update mempool_monitor to use centralized logging setup and improve logger initialization

--- python/mempool_monitor.py
+++ python/mempool_monitor.py
@@ -1,5 +1,5 @@
 import asyncio
-import logging
+import logging as logger
 import time
 import async_timeout
 import hexbytes as hexbytes
@@ -10,15 +10,18 @@ from web3 import AsyncWeb3
 from web3.exceptions import TransactionNotFound
 from web3.exceptions import Web3ValueError
 
-from utils import getLogger
 from abi_registry import ABI_Registry
 from api_config import API_Config
 from configuration import Configuration
 from market_monitor import Market_Monitor
 from nonce_core import Nonce_Core
 from safety_net import Safety_Net
-import logging
-logger = getLogger("0xBuilder", level=logging.INFO)
+import logging as logger
+from main_core import setup_logging
+
+setup_logging()
+
+logger = logger.getLogger(__name__)
 
 class Mempool_Monitor:
     """
